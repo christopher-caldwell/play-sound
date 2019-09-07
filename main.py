a37e14a972b85f946f1e09f7f692d02abd319bba
caldwell619 <caldwell8814@gmail.com>
2019-09-06 20:38:35 -0500

    removing playsound

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from playsound import playsound  # imports method from object package
 import click  # imports object that contains methods
 from sound_list import sound_list  # contains the dict of songs available
+from pygame import mixer
 
-
+mixer.init()
 @click.command()
 @click.option("--song-number", 'song',
               prompt="Number of audio file",
@@ -12,8 +12,9 @@ def play_selected_sound(song):
     """Press a button, hear the beep"""
 
     while song != 0:
-# cast the song int to a string, use it as the key to a hash map dictionary to grab the file path
-        playsound(sound_list[(str(song))])
+        # cast the song int to a string, use it as the key to a hash map dictionary to grab the file path
+        mixer.music.load(sound_list[(str(song))])
+        mixer.music.play()
         play_selected_sound()
     exit
 
